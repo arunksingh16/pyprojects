d7fda9ed94595579b7a42865f60401c43f3d4bab
Arun Kumar Singh <arun.singh1@contractor.axaxl.com>
2021-02-01 11:52:21 +0000

    flask blog dummy project

---
+++ flask-blog/init_db.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+cur.execute("INSERT INTO restservicedb (fname, lname) VALUES (?, ?)",
+            ('Arun', 'Singh')
+            )
+
+cur.execute("INSERT INTO restservicedb (fname, lname) VALUES (?, ?)",
+            ('Rumanshi', 'Ahuja')
+            )
+
+connection.commit()
+connection.close()
